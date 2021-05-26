68869a0f957846fcdc0c5e2d69557853de5c8eec
pariwesh009 <ajay2322020@outlook.com>
2021-05-26 19:51:33 +0530

    added Eureka

--- BugProject/src/main/java/com/hexaware/BugProjectApplication.java
+++ BugProject/src/main/java/com/hexaware/BugProjectApplication.java
@@ -2,15 +2,17 @@ package com.hexaware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class BugProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BugProjectApplication.class, args);
 	}
-
 }
