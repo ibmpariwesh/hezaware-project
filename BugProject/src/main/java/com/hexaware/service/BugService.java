9652b97ad0d8da9faccccbc41705b77dcddd2eb0
pariwesh009 <ajay2322020@outlook.com>
2021-05-26 22:03:12 +0530

    added Hystrix

--- BugProject/src/main/java/com/hexaware/service/BugService.java
+++ BugProject/src/main/java/com/hexaware/service/BugService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hexaware.entity.BugRequest;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixException;
 
 @Service
 public class BugService {
@@ -15,12 +17,17 @@ public class BugService {
 	EmailClient emailClient;
 	@Transactional(rollbackOn = Exception.class, dontRollbackOn = { ArithmeticException.class,
 			IllegalArgumentException.class })
+	@HystrixCommand(fallbackMethod = "fallback1")
 	public void create(BugRequest bugRequest) throws Exception {
 		System.out.println(bugRequest);
 //		childMethod(bugRequest);
 		bugRepository.save(bugRequest);
 		emailClient.sendEmail(bugRequest);
 //		throw new Exception();
+	}
+	
+	public void fallback1(BugRequest bugRequest) {
+		System.out.println("fallback called...");
 	}
 //	@Transactional(value = TxType.REQUIRES_NEW)
 //	public void childMethod(BugRequest bugRequest) {
--- BugProject/src/main/java/com/hexaware/service/EmailClient.java
+++ BugProject/src/main/java/com/hexaware/service/EmailClient.java
@@ -5,9 +5,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.hexaware.entity.BugRequest;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @FeignClient(name="email-service")
 public interface EmailClient {
 	@PostMapping("/email")
+	
 	void sendEmail(@RequestBody BugRequest bug);
 }
